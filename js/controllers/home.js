52deb95071c72b4aa4ea18a4b5e3eade0a75be0f
易德 <yide@cnhuqi.com>
2017-12-27 14:57:14 +0800

    * 修改页面架构

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.controller('home-controller', ['$scope', function ($scope) {
+app.controller('home-controller', ['$scope', '$rootScope', function ($scope, $rootScope) {
     $scope.content = 'This is home page';
-    console.log('111');
+    console.log($rootScope);
 }]);
